7b0ad2780bb2ee73934c0595fea85c85e21dbd57
Sofia Garcia <sofisagarcia@gmail.com>
2016-09-20 00:14:09 -0400

    boundaries/hotspots have been set; no painting functionality yet

--- paint-app-v1/sketch.js
+++ paint-app-v1/sketch.js
@@ -1,28 +1,30 @@
 var circle = {
-	red : {x: 100, col: "#e54341"},
-	green: {x: 200, col: "#60b896"},
-	lilac: {x: 300, col: "#9b84af"},
-	yellow: {x: 400, col: "#f7b320" },
-	random: {x: 500, col: "#ffffff"},
+	red : {x: 100, col: "#e54341", leftSide: 60, rightSide: 140},
+	green: {x: 200, col: "#60b896", leftSide: 160, rightSide: 240},
+	lilac: {x: 300, col: "#9b84af", leftSide: 260, rightSide: 340},
+	yellow: {x: 400, col: "#f7b320", leftSide: 360, rightSide: 440 },
+	random: {x: 500, col: "#ffffff", leftSide: 460, rightSide: 540},
 	y: 500,
 	diameter: 80,
   radius: 40
 };
 
-// var areaColor = [ ];
+var boundary = {
+	top : circle.y - circle.radius,
+	bottom: circle.y + circle.radius
+};
+
+var currentColor = " ";
 // var topBoundary = circle.y - circle.radius;
-// var leftBoundary = circle.x - circle.radius;
-// var rightBoundary = circle.x + circle.radius
+// var leftBoundary = circle.red.x - circle.radius;
+// var rightBoundary = circle.red.x + circle.radius
 // var bottomBoundary = circle.y + circle.radius;
 
 function setup() {
 	createCanvas(600, 600);
-	// areaColor= ["#e54341", "#60b896","#9b84af","#f7b320","#537bbe"];
 }
 
-// for(var i = 0; i < 6; i++){
-// 	circleX.push(circle.x + 1);
-// }
+
 function draw() {
 background(2);
 
@@ -43,10 +45,25 @@ fill(circle.random.col);//random (grey scale)
 ellipse(circle.random.x, circle.y, circle.diameter, circle.diameter);
 
 //
-// if(mouseY > topBoundary && mouseY < bottomBoundary){
-//  if(mouseX > leftBoundary && mouseX < rightBoundary ){
-//   console.log('Red Area');
-//  }
-// }
+
+if(mouseY > boundary.top && mouseY < boundary.bottom){
+	if(mouseX > circle.red.leftSide && mouseX < circle.red.rightSide ){
+			currentColor = "red";
+			console.log(currentColor);
+	} if(mouseX > circle.green.leftSide && mouseX < circle.green.rightSide ){
+				currentColor = "green";
+				console.log(currentColor);
+	} if(mouseX > circle.lilac.leftSide && mouseX < circle.lilac.rightSide ){
+				currentColor = "lilac";
+				console.log(currentColor);
+	} if(mouseX > circle.yellow.leftSide && mouseX < circle.yellow.rightSide ){
+				currentColor = "yellow";
+				console.log(currentColor);
+	} if(mouseX > circle.random.leftSide && mouseX < circle.random.rightSide ){
+				currentColor = "random";
+				console.log(currentColor);
+	}
+}
+
 
 }
